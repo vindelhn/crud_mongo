b896203c5c2a421429aae28bf2cdb9e131e325b9
Alexander Vindel <30450283+vindelhn@users.noreply.github.com>
2024-11-22 15:37:11 -0600

    * Creacion de estructura inicial del proyecto backend

---
+++ backend_mongo/src/main/java/com/demo/backend_mongo/BackendMongoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.backend_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMongoApplication.class, args);
+    }
+
+}
---
+++ backend_mongo/src/test/java/com/demo/backend_mongo/BackendMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.backend_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
